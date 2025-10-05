476c317ed0720be6b3e6a39591c8c7bfabb26357
Lion Kortlepel <development@kortlepel.com>
2025-10-05 12:41:35 +0200

    chore: remove "Error" string where it doesnt belong

--- src/error.rs
+++ src/error.rs
@@ -8,7 +8,7 @@ pub enum NodeError {
     Generic(String),
     #[error("Error in Node {0}: {1}")]
     GenericInNode(usize, Box<dyn std::error::Error>),
-    #[error("Error: {0}")]
+    #[error("{0}")]
     WithInner(Box<dyn std::error::Error>),
 }
 pub type Result<T> = std::result::Result<T, NodeError>;
