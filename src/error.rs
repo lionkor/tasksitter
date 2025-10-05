2a40f3fb67e875b4cde5deee99cf7736ad4da050
Lion Kortlepel <development@kortlepel.com>
2025-10-05 12:28:59 +0200

    feat: add error type 'WithInner' and fix 'GenericInNode'

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,8 @@ pub enum NodeError {
     #[error("Generic error: {0}")]
     Generic(String),
     #[error("Error in Node {0}: {1}")]
-    GenericInNode(usize, Box<NodeError>),
+    GenericInNode(usize, Box<dyn std::error::Error>),
+    #[error("Error: {0}")]
+    WithInner(Box<dyn std::error::Error>),
 }
 pub type Result<T> = std::result::Result<T, NodeError>;
